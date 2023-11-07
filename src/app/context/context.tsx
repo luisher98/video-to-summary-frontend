3146318e3e0668f75890082bb4793c83b8b9afa7
luisher98 <luisheratm@gmail.com>
2023-11-07 15:55:47 +0000

    added data.json for emulating api

--- src/app/context/context.tsx
+++ src/app/context/context.tsx
@@ -2,13 +2,21 @@ import {createContext, useContext, useState} from "react";
 
 const Context = createContext({});
 
+
+function getSummary(url) {
+    return fetch(`http://localhost:5000/api/summary?url=${url}`)
+        .then(response => response.json())
+        .then(data => data.summary);
+}
+
+
 export function ContextProvider({children}) {
     const [input, setInput] = useState("");
-    const [video, setVideo] = useState("");
+ 
     const [summary, setSummary] = useState("");
 
     return (
-        <Context.Provider value={{input, setInput, video, setVideo, summary, setSummary}}>
+        <Context.Provider value={{input, setInput, summary, setSummary}}>
             {children}
         </Context.Provider>
     );
