04dffb6b3be67b7793dff10cfdff7a5bb76809d5
luisher98 <luisheratm@gmail.com>
2023-11-07 16:25:30 +0000

    modified FetchSummary.tsx to temporarily support json-server

--- src/app/utils/FetchSummary.tsx
+++ src/app/utils/FetchSummary.tsx
@@ -1,11 +1,12 @@
-export default async function getSummary(url) {
+export default async function getSummary(url = "") {
+  const API_URL = "http://localhost:3500/videoSummary";
   try {
     // API call to the backend (using json-server instead for now)
     // const response = await fetch(
     //   `http://localhost:5000/api/summary?url=${url}`,
     // );
 
-    const response = await fetch("")
+    const response = await fetch(API_URL);
 
     if (!response.ok) {
       throw new Error("Network response failed");
@@ -15,7 +16,6 @@ export default async function getSummary(url) {
     return data;
   } catch (error) {
     console.error("Error:", error);
-    // Handle the error as needed
-    return null; // You can return null or an error message
+    return null;
   }
 }
